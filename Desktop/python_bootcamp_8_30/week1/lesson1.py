f6d17e95ab3b179256e8e63d03ab488538447d1a
Asylbekovich <Aidartashtanbekov524@gmail.com>
2022-06-29 09:43:04 +0600

    if else elif

---
+++ Desktop/python_bootcamp_8_30/week1/lesson1.py
@@ -0,0 +1,63 @@
+#lists = [1, "jfhsj", 2.3, [1, 2], {"s": 3}]
+#str_1 ="hello world"
+#for i in range(10):
+    #print(i)
+#print("len = ", len(lists))
+
+#for i in range(0, 101, 2):
+  #  print(i)
+#lits_1 = list(range(0,101, 2))
+#print(lits_1)
+
+#append()
+#lists = [1, "jfhsj", 2.3, [1, 2], {"s": 3}, ]
+#lists.append({"name": "Nazgul"})
+#lists.append([1, 3, ])
+#lists.append([1, 3, ])
+#lists.extend([1, 3, ])
+#print(lists[1], "before")
+#lists.insert(1, 5)
+#print(lists[1], "after")
+#print(lists)
+
+#pop()
+#phone_numbers = ['99675501099', "996990037333", '996555018665']
+#black_list = phone_numbers.pop(2)
+#print(black_list)
+#print(phone_numbers)
+#phone_numbers.insert(0, black_list)
+#print(phone_numbers)
+
+#remove()
+
+#phone_numbers = ['99675501099', "996990037333", '996555018665']
+#phone_numbers.remove("996990037333")
+#print(phone_numbers.count("996755001099"))
+#phone_numbers.reverse()
+#print(phone_numbers)
+#start = int(input("start = "))
+#end = int(input("end = "))
+#step = int(input("step = "))
+#power = int(input("power = "))
+#empty = []
+#for i in range(start, end, step):
+#    empty.append(i**power)
+#print(empty)
+#empty.clear()
+#print(empty)
+#empty = [1, 2, 3, 2]
+#empty.sort()
+#print(empty)
+
+#this is koment
+#num = 123
+#pi = 3.14
+#t = [1, 3.2, "name"]
+#name = "Aidar"
+#print(name) # it is name
+#print(dir(name))
+#print(type(name))
+#print(name.upper())
+#print(name.capitalize())
+#print(name.title())
+
---
+++ Desktop/python_bootcamp_8_30/week1/lesson2.py
@@ -0,0 +1,66 @@
+#условные оераторы
+#a = [2, 3, 4]
+#s = "string"
+#a.reverse()
+#s.lower()
+#print(a)
+#print(s.lower())
+#if
+#if 4 > 2:
+ #   print("4 more 2")
+#  num1 = int(input("num1: "))
+#  num2 = int(input("num2: "))
+#
+#  if num1 > num2:
+#      print("{0} grater {1}".format(num1, num2))
+#  elif num2 > num1:
+# print("{0} grater {1}".format(num1, num2))
+# num1 = int(input("num1: "))
+# num2 = int(input("num2: "))
+# operator = input("operator: ")
+# if operator == "+":
+#     result = num1 + num2
+#     print("result = {}".format(result))
+#
+# elif operator == '-':
+#     result = num1 - num2
+#     print("result = {}".format(result))
+#
+# elif operator == "*":
+#     result = num1 * num2
+#     print("result = {}".format(result))
+#
+# else:
+#     result = num1 / num2
+#     print("result = {}".format(result))
+
+#lists = [1, 2]
+#lists = [1, 2, 3, 4]
+
+#if 3 in lists:
+ #   print("Hello")
+
+# or and
+#lists = [18, 22, 25, 6]
+# if (22 in lists) and (25 in lists):
+#     print("yes")
+# lists_2 = list()
+# lists_1 = list(range(1, 1001))
+# for i in lists_1:
+#     if i % 3 == 0 and i % 5 == 0 and i % 15 == 0:
+#         lists_2.append(i)
+# print(lists_2):
+
+# temp = float(input('Введите темпеатуру: '))
+# farangeyt = input("farangeyt: ")
+# if farangeyt == "farenn":
+#     print(farangeyt * 9 / 5 + 32, "по Фарангейту")
+# elif farangeyt == "temp":
+#   print(farangeyt * 5 / 9 + 32, "По фарангейту")
+# #
+#
+# for i in range(11):
+#     print(2, "*", i, '=', 2 * i)
+
+
+
